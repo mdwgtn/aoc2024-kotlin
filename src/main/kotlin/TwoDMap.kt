6714c51c7c6c5ed1203e5fed778ba71eb7cacda5
Mike Davidson <md.wgtn@gmail.com>
2024-12-04 12:43:56 -0400

    refactor Day 4

--- src/main/kotlin/TwoDMap.kt
+++ src/main/kotlin/TwoDMap.kt
@@ -19,16 +19,20 @@ class Coordinates(val x: Int, val y: Int) {
 
 class TwoDMap(private val input: List<String>) {
 
-    private val height: Int = input.size
-    private val width: Int = input[0].length
-    private val coordinates = allCoordinates()
+    val height: Int = input.size
+    val width: Int = input[0].length
+    val coordinates = allCoordinates()
 
-    private fun at(pos: Coordinates): Char = input[pos.y][pos.x]
+    fun at(pos: Coordinates): Char = input[pos.y][pos.x]
 
-    private fun neighbour(pos: Coordinates, direction: Int = 5): Coordinates? {
+    fun neighbour(pos: Coordinates, direction: Int = 5): Coordinates? {
         val n = pos.neighbour(direction)
         return if (locationExists(n)) n else null
     }
+
+    fun isOnAnEdge(pos: Coordinates): Boolean = (pos.x == 0 || pos.x == width - 1 || pos.y == 0 || pos.y == height - 1)
+
+    private fun locationExists(pos: Coordinates?): Boolean = pos != null && pos.x in 0..<width && pos.y in 0..<height
 
     private fun allCoordinates(): List<Coordinates> {
         val result = mutableListOf<Coordinates>()
@@ -39,57 +43,4 @@ class TwoDMap(private val input: List<String>) {
         }
         return result;
     }
-
-    private fun locationExists(pos: Coordinates?): Boolean = pos != null && pos.x in 0..<width && pos.y in 0..<height
-
-
-    private fun hasDirectionalLinearSequenceFrom(sequence: String, pos: Coordinates, direction: Int): Boolean {
-        if (sequence.isEmpty()) return true
-        if (at(pos) == sequence[0]) {
-            if (sequence.length == 1) {
-                return true
-            }
-            val n = neighbour(pos, direction)
-            return if (n == null) false else hasDirectionalLinearSequenceFrom(sequence.substring(1), n, direction)
-        }
-        return false
-    }
-
-    private fun countOfLinearSequencesFrom(sequence: String, pos: Coordinates): Int {
-        return listOf(1, 2, 3, 4, 6, 7, 8, 9).count {
-            hasDirectionalLinearSequenceFrom(sequence, pos, it)
-        }
-    }
-
-    fun countOfLinearSequences(sequence: String): Int =
-        coordinates.sumOf { countOfLinearSequencesFrom(sequence, it) }
-
-    private fun hasXWordCentredAt(pos: Coordinates, centreLetter: Char, firstLetter: Char, lastLetter: Char): Boolean {
-        val edgeLetters = listOf(firstLetter, lastLetter)
-        if (at(pos) != centreLetter) {
-            return false
-        }
-        val topLeft = neighbour(pos, 7)
-        val topRight = neighbour(pos, 9)
-        val bottomLeft = neighbour(pos, 1)
-        val bottomRight = neighbour(pos, 3)
-        if (!listOf(topLeft, topRight, bottomLeft, bottomRight).all { locationExists(it) }) {
-            return false
-        }
-        val topLeftLetter = at(topLeft!!)
-        val topRightLetter = at(topRight!!)
-        val bottomLeftLetter = at(bottomLeft!!)
-        val bottomRightLetter = at(bottomRight!!)
-        val actualEdgeLetters = listOf(topLeftLetter, topRightLetter, bottomLeftLetter, bottomRightLetter)
-
-        // the edge letters must appear twice each
-        if (!(edgeLetters.all { 2 == actualEdgeLetters.count { actual -> it == actual } })) {
-            return false
-        }
-        // filter out SAS and MAM, otherwise this is a match
-        return !(topLeftLetter == bottomRightLetter || topRightLetter == bottomLeftLetter);
-    }
-
-    fun countOfXWords(sequence: String): Int =
-         coordinates.count { hasXWordCentredAt(it, sequence[1], sequence[0], sequence[2])  }
 }
--- src/main/kotlin/solutions/Day04.kt
+++ src/main/kotlin/solutions/Day04.kt
@@ -1,5 +1,6 @@
 package solutions
 
+import Coordinates
 import TwoDMap
 import solve
 import test
@@ -17,11 +18,64 @@ fun main() {
 }
 
 class XmasCount(input: List<String>) {
-    val map = TwoDMap(input)
+    private val map = TwoDMap(input)
     fun part1(): Int {
-        return map.countOfLinearSequences("XMAS")
+        return countOfLinearSequences("XMAS")
     }
+
     fun part2(): Int {
-        return map.countOfXWords("SAM")
+        return countOfXWords("SAM")
     }
+
+    private fun hasDirectionalLinearSequenceFrom(sequence: String, pos: Coordinates, direction: Int): Boolean {
+        if (sequence.isEmpty()) {
+            return true
+        }
+        with(map) {
+            if (at(pos) != sequence[0]) {
+                return false
+            }
+            if (sequence.length == 1) {
+                return true
+            }
+            val n = neighbour(pos, direction)
+            return if (n == null) false else hasDirectionalLinearSequenceFrom(sequence.substring(1), n, direction)
+        }
+    }
+
+    private fun countOfLinearSequencesFrom(sequence: String, pos: Coordinates): Int {
+        return listOf(1, 2, 3, 4, 6, 7, 8, 9).count {
+            hasDirectionalLinearSequenceFrom(sequence, pos, it)
+        }
+    }
+
+    private fun countOfLinearSequences(sequence: String): Int =
+        map.coordinates.sumOf { countOfLinearSequencesFrom(sequence, it) }
+
+    private fun hasXWordCentredAt(pos: Coordinates, centreLetter: Char, firstLetter: Char, lastLetter: Char): Boolean {
+        val edgeLetters = listOf(firstLetter, lastLetter)
+        with(map) {
+            if (at(pos) != centreLetter || isOnAnEdge(pos)) {
+                return false
+            }
+
+            val actualEdgeLetters = listOf(
+                at(neighbour(pos, 7)!!),
+                at(neighbour(pos, 9)!!),
+                at(neighbour(pos, 1)!!),
+                at(neighbour(pos, 3)!!)
+            )
+
+            // the edge letters must appear twice each
+            if (!(edgeLetters.all { 2 == actualEdgeLetters.count { actual -> it == actual } })) {
+                return false
+            }
+            // filter out SAS and MAM, otherwise this is a match
+            return !(actualEdgeLetters[0] == actualEdgeLetters[3] || actualEdgeLetters[1] == actualEdgeLetters[2]);
+        }
+    }
+
+    private fun countOfXWords(sequence: String): Int =
+        map.coordinates.count { hasXWordCentredAt(it, sequence[1], sequence[0], sequence[2]) }
+
 }
